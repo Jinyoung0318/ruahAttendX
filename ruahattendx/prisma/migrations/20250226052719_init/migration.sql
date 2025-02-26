7774bf390fa24a3b1ecc24c973be27fcfa2bc1f7
jinyoungHur <gjwlsdud0318@gmail.com>
2025-02-26 14:31:26 +0900

    [db] prisma / redirect
    [db] prisma connect / example use with prisma / redirect page

---
+++ ruahattendx/prisma/migrations/20250226052719_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `RAX_U_TEST` on the `RAX_USER` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `RAX_USER` DROP COLUMN `RAX_U_TEST`;
---
+++ ruahattendx/src/app/pages/page.tsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <p>로그인 페이지에 오신 것을 환영합니다.</p>
+        </div>
+    );
+}
