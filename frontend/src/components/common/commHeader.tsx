fea1566fd1de57d5f4086514920af8d091de492f
허진영 <jy.hur@okestro.com>
2025-04-21 16:23:10 +0900

    change theme3 && add logout logic

--- frontend/src/components/common/commHeader.tsx
+++ frontend/src/components/common/commHeader.tsx
@@ -1,23 +1,44 @@
 import styles from '../../styles/header.module.css';
-import {useLocation} from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useState } from 'react';
 
 const userName = JSON.parse(sessionStorage.getItem('user') || '{}').userName;
 const userParName = JSON.parse(sessionStorage.getItem('user') || '{}').userParName;
 
 const CommHeader = () => {
     const location = useLocation();
-    const pageTitle = location.state?.pageTitle || '';
+    const pageTitle = location.state?.pageTitle || '출석률 현황 📈';
+    const navigate = useNavigate();
+    const [showDropdown, setShowDropdown] = useState(false);
 
     return (
         <div className={styles.header}>
             <h1 className={styles.title}>{pageTitle}</h1>
-            <div className={styles.userInfo}>
+            <div
+                onClick={() => setShowDropdown(!showDropdown)}
+                className={styles.userInfo}
+                style={{ cursor: 'pointer'}}
+            >
                 <img
                     src="https://randomuser.me/api/portraits/men/1.jpg"
                     alt="User"
                     className={styles.avatar}
                 />
                 <span className={styles.userName}>{userName} {userParName}</span>
+                {showDropdown && (
+                    <div className={styles.dropdownPanel}>
+                        <div
+                            className={styles.dropdownItem}
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                sessionStorage.clear();
+                                navigate('/login');
+                            }}
+                        >
+                            로그아웃
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
